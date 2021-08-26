d67d7a95f1e486df38a45ff90bd46b1f77260a7d
KikiPadoru <61500808+KikiPadoru@users.noreply.github.com>
2021-08-26 20:14:10 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,14 +1,11 @@
 def sort_list(mass):
-    if len(mass)==0: return mass
-    min_m = min(mass)
-    max_m = max(mass)
-    for i in range(0,len(mass)) :
-        if mass[i]==min_m:
-            mass[i] = max_m
-            continue
-        if mass[i]==max_m:
-            mass[i] = min_m
-    mass.append(min_m)
+    if len(mass)!=0:
+        min_m = min(mass)
+        max_m = max(mass)
+        mass = [' ' if e == min_m else e for e in mass]
+        mass = [min_m if e == max_m else e for e in mass]
+        mass = [max_m if e == ' ' else e for e in mass]
+        mass.append(min_m)
     return mass
 
 print(sort_list([]))
