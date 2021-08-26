c66c3cb0a052884f5cd65b8f9537ba9991e681ab
KikiPadoru <61500808+KikiPadoru@users.noreply.github.com>
2021-08-26 20:15:28 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -3,7 +3,7 @@ class NoSuchStrategyError(Exception): pass
 class OnePlayerError(Exception): pass
 class ZeroPlayersError(Exception): pass
 def rps_game_winner(mass):
-    move = ['P', 'R', 'S']
+    win_move = {'P': 'R', 'R': 'S', 'S': 'P'}
     try:
         if type(mass) is not list:
             raise TypeError
@@ -13,15 +13,12 @@ def rps_game_winner(mass):
             raise OnePlayerError
         elif len(mass)==0:
             raise ZeroPlayersError
-        if mass[0][1] not in move or mass[1][1] not in move:
+        if mass[0][1] not in win_move or mass[1][1] not in win_move:
             raise NoSuchStrategyError
     except WrongNumberOfPlayersError:
         print('\033[31mWrongNumberOfPlayersError\033[0m')
     except OnePlayerError:
-        if mass[0][1] not in move:
-            print('\033[31mNoSuchStrategyError\033[0m')
-        else:
-            print(mass[0][0],' ',mass[0][1])
+        print((f'{mass[0][0]} {mass[0][1]}','\033[31mNoSuchStrategyError\033[0m')[mass[0][1] not in win_move])
     except ZeroPlayersError:
         print('\033[31mZeroPlayersError\033[0m')
     except NoSuchStrategyError:
@@ -30,22 +27,9 @@ def rps_game_winner(mass):
         print('\033[41mTypeError\033[0m')
     else:
         if mass[0][1]==mass[1][1]:
-            print(mass[0][0],' ',mass[0][1])
-        elif mass[0][1]=='P':
-            if mass[1][1] =='R':
-                print(mass[0][0],' ',mass[0][1])
-            else:
-                print(mass[1][0], ' ', mass[1][1])
-        elif mass[0][1] == 'R':
-            if mass[1][1] =='S':
-                print(mass[0][0],' ',mass[0][1])
-            else:
-                print(mass[1][0], ' ', mass[1][1])
-        elif mass[0][1] == 'S':
-            if mass[1][1] =='P':
-                print(mass[0][0],' ',mass[0][1])
-            else:
-                print(mass[1][0], ' ', mass[1][1])
+            print(mass[0][0],mass[0][1])
+        else:
+            print((f'{mass[1][0]} {mass[1][1]}',f'{mass[0][0]} {mass[0][1]}')[win_move[mass[0][1]]==mass[1][1] ])
 
 
 
@@ -55,5 +39,5 @@ rps_game_winner([['player1', 'P'], ['player2', 'S'], ['player3', 'S']])
 rps_game_winner([['player1', 'P'], ['player2', 'A']])
 rps_game_winner([['player1', 'P'], ['player2', 'S']])
 rps_game_winner([['player1', 'P'], ['player2', 'P']])
-rps_game_winner([['player2', 'A']])
+rps_game_winner([['player2', 'P']])
 rps_game_winner([])
