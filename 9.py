16f92dd533b467041adf118c78f70a05edfbe821
KikiPadoru <61500808+KikiPadoru@users.noreply.github.com>
2021-08-26 20:42:43 +0300

    Update 9.py

--- 9.py
+++ 9.py
@@ -4,24 +4,14 @@ def more_return(my_dict,new_dict):
             new_dict[k]=v
     return new_dict
 
-
 def connect_dicts(dict1,dict2):
-    a =int()
-    b = int()
-    for i in dict1.values(): a += i
-    for i in dict2.values(): b += i
-    new_dict = dict()
-    if a>b :
-        new_dict = more_return(dict2,new_dict)
-        new_dict = more_return(dict1,new_dict)
-    else:
-        new_dict = more_return(dict1,new_dict)
-        new_dict = more_return(dict2, new_dict)
+    a = sum(dict1.values())
+    b = sum(dict2.values())
+    new_dict = more_return(dict1, more_return(dict2,new_dict=dict()))if a>b else more_return(dict2, more_return(dict1,new_dict=dict()))
     sorted_dict = {}
     sorted_keys = sorted(new_dict, key=new_dict.get)
     for w in sorted_keys:
         sorted_dict[w] = new_dict[w]
-
     return sorted_dict
 
 print(connect_dicts({ "a": 2, "b": 12 }, { "c": 11, "e": 5 }))
