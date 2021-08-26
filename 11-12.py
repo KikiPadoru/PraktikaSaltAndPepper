18706b70ea36338df65ca4646417ddd53d53a90d
KikiPadoru <61500808+KikiPadoru@users.noreply.github.com>
2021-08-26 20:45:08 +0300

    Update 11-12.py

--- 11-12.py
+++ 11-12.py
@@ -2,13 +2,17 @@ class Dessert:
     def __init__(self,name='',calories=0):
         self.__name = name
         self.__calories = calories
-    def get_name(self):
+    @property
+    def name(self):
         return self.__name
-    def get_calories(self):
-        return self.__calories
-    def set_name(self,name):
+    @name.setter
+    def name(self,name):
         self.__name = name
-    def set_calories(self,calories):
+    @property
+    def calories(self):
+        return self.__calories
+    @calories.setter
+    def calories(self,calories):
         self.__calories = calories
     def is_healthy(self):
         if(self.__calories<200): return True
@@ -17,15 +21,19 @@ class Dessert:
         return True
     def __str__(self):
         return f'name: {self.__name}, calories: {self.__calories}'
+
 class JellyBean(Dessert):
     def __init__(self, name='', calories=0, flavor=''):
         super(JellyBean, self).__init__(name, calories)
         self.__flavor = flavor
     def __str__(self):
         return super(JellyBean, self).__str__()+f', flavor: {self.__flavor}'
-    def get_flavor(self):
+
+    @property
+    def flavor(self):
         return self.__flavor
-    def set_flavor(self,flavor):
+    @flavor.setter
+    def flavor(self,flavor):
         self.__flavor = flavor
     def is_delicious(self):
         if self.__flavor == 'black licorice': return False
@@ -35,5 +43,5 @@ class JellyBean(Dessert):
 des = Dessert('Мороженка', 100)
 d = JellyBean('Мармелад', 150 , 'black licorice')
 print(d)
-print(d.get_name())
+print(d.name)
 print(d.is_delicious())
