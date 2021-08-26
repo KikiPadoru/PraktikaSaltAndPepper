1f038663ba7cb07ff93e26be3e28d4c493cc944f
KikiPadoru <61500808+KikiPadoru@users.noreply.github.com>
2021-08-26 20:44:10 +0300

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,18 +1,10 @@
 import re
 def count_words(words):
-    words = re.sub(r'[^\w\s]', '', words)
-    words = words.lower()
-    print(words)
-    new_words = words.split(' ')
-    while True:
-        try: new_words.remove('')
-        except: break
+    words = re.sub(r'[^\w\s+]', '', words).replace('  ',' ').lower().split(' ')
     new_dict = dict()
-    for i in new_words:
-        if i in new_dict:
-            new_dict[i] += 1
-        else:
-            new_dict[i] = 1
+    for i in words:
+        new_dict[i] = new_dict[i]+1 if i in new_dict else 1
     return new_dict
+
 print(count_words("A man, a plan, a canal -- Panama"))
 print(count_words("Doo bee doo bee doo"))
